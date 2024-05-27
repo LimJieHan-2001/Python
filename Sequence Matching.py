10f7daf596edd0fae8da01da05f6bdeb7b5e6754
Lim Jie Han <jiehan.lim-2001@graduate.utm.my>
2024-05-27 18:53:03 +0800

    commit msg

--- Sequence Matching.py
+++ Sequence Matching.py
@@ -1,35 +1,33 @@
-def readGenome(lambda_virus.fa):
+def read_Genome(filename):
     genome = ''
-    with open(lambda_virus.fa, 'r') as f:
+    with open(filename, 'r') as f:
         for line in f:
-            if not line[0] == '>':
+            if not line.startswith('>'):
                 genome += line.rstrip()
     return genome
 
 def naive_with_mismatches(p, t, max_mismatches):
     occurrences = []
     for i in range(len(t) - len(p) + 1):
-        match = True
         mismatches = 0
         for j in range(len(p)):
-            if t[i+j] != p[j]:
+            if t[i + j] != p[j]:
                 mismatches += 1
                 if mismatches > max_mismatches:
-                    match = False
                     break
-        if match:
+        if mismatches <= max_mismatches:
             occurrences.append(i)
     return occurrences
 
-def reverseComplement(s):
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+def reverse_Complement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     t = ''
     for base in s:
         t = complement[base] + t
     return t
 
 # Reading the genome from the file
-genome = readGenome('lambda_virus.fa')
+genome = read_Genome('lambda_virus.fa')
 
 # Define the pattern and the maximum number of mismatches
 pattern = 'AGTCGA'  # Example pattern
@@ -37,7 +35,9 @@ max_mismatches = 2  # Example maximum number of mismatches
 
 # Find occurrences in the genome
 occurrences = naive_with_mismatches(pattern, genome, max_mismatches)
-reverse_pattern = reverseComplement(pattern)
+
+# Find occurrences in the reverse complement of the genome
+reverse_pattern = reverse_Complement(pattern)
 reverse_occurrences = naive_with_mismatches(reverse_pattern, genome, max_mismatches)
 
 # Combine occurrences
