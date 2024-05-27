f4aad2189e505d614ef56f31fa5a12d9101975eb
Lim Jie Han <jiehan.lim-2001@graduate.utm.my>
2024-05-27 18:08:08 +0800

    commit msg

---
+++ Genome.py
@@ -0,0 +1,27 @@
+def readGenome(lambda_virus.fa):
+    genome = ''
+    with open(lambda_virus.fa, 'r') as f:
+        for line in f:
+            # ignore header line with genome information
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+def naive(p, t):
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):  # loop over alignments
+        match = True
+        for j in range(len(p)):  # loop over characters
+            if t[i+j] != p[j]:  # compare characters
+                match = False
+                break
+        if match:
+            occurrences.append(i)  # all chars matched; record
+    return occurrences
+
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
