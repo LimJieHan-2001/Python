86b373f67ab62a11a9aa45b302f9c4781c0abd22
Lim Jie Han <jiehan.lim-2001@graduate.utm.my>
2024-06-19 16:09:55 +0800

    commit msg

--- import_data.py
+++ import_data.py
@@ -11,19 +11,19 @@ interactions_collection = db["interactions"]
 organisms_collection = db["organisms"]
 
 # Import Proteins Data
-with open('proteins.csv', 'r') as file:
+with open('Protein_Information.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         proteins_collection.insert_one(row)
 
 # Import Interactions Data
-with open('interactions.csv', 'r') as file:
+with open('Interaction_Data.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         interactions_collection.insert_one(row)
 
 # Import Organisms Data
-with open('organisms.csv', 'r') as file:
+with open('Organism_Information.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         organisms_collection.insert_one(row)
--- main.py
+++ main.py
@@ -26,3 +26,4 @@ async def get_organism_proteins(organism_id: str):
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=8000)
+
